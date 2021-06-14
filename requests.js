de84de47e0271390d8147cabc81f19425abd64c1
ilwYennefer <javierf7fywmrp@gmail.com>
2021-06-14 19:11:31 +0400

    added API_KEY

--- requests.js
+++ requests.js
@@ -1,5 +1,4 @@
-// const API_KEY = process.env.API_KEY;
-const API_KEY = "1deb1e4cf26d25923913bbe241c8cb45";
+const API_KEY = process.env.API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
